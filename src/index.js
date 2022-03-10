65a68612096bf1f1f5bfc81a006a2bf770766acf
difagume <difagume@gmail.com>
2022-03-10 12:22:56 -0500

    corrección de Uri

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:400https://apollo-odyssey-lift-server.herokuapp.com/',
+  uri: 'https://apollo-odyssey-lift-server.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
